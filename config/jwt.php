e16dbc83432e60b671f67bf8b15d364de20117a2
Kedar V <kedar54@gmail.com>
2016-12-16 13:07:14 -0500

    add travis CI config

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'DOBwIPWSuiTl4YnzyuhCQv0tOWjFyc0j'),
+    'secret' => env('JWT_SECRET'),
 
     /*
     |--------------------------------------------------------------------------
